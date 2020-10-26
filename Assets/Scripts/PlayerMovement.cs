ba3686a25c6c88bffb849baae5e04eba7150075a
TiagoMAGarcia <73131074+TiagoMAGarcia@users.noreply.github.com>
2020-10-26 17:35:01 +0000

    reset2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public CharacterController2D controller;
+    public Animator animator;
+
     float horizontalMove = 0f;
+    
     public float runSpeed = 40f;
     bool jump = false;
     bool crouch = false;
-
+    
     // Update is called once per frame
     void Update() {
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    
         if(Input.GetButtonDown("Jump")){
             jump = true;
+            animator.SetBool("isJumping", true);
         }
         if(Input.GetButtonDown("Crouch")){
             crouch = true;
         } else if (Input.GetButtonUp("Crouch")) {
             crouch = false;
         }
+    }
+
+    public void onLanding() {
+        animator.SetBool("isJumping", false);
     }
 
     void FixedUpdate() {
