2407b18a5dd02802d28f6b8d3322b74cdfc0754e
FilipeABBarroso <filipebarroso2812@gmail.com>
2020-10-29 00:26:17 +0000

    Crab movement fix

--- Assets/Scripts/CrabMov.cs
+++ Assets/Scripts/CrabMov.cs
@@ -3,47 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CrabMov : MonoBehaviour{
-    
-    /*public float moveSpeed;
-    [HideInInspector] public bool mustPatrol;
-    private bool mustTurn;
-    public Rigidbody2D myRigidbody;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
-    public Collider2D bodyCollider;
-
-
-    void Start(){
-        mustPatrol = true;
-    }
-
-    void Update(){
-        if(mustPatrol){
-            Patrol();
-        }
-    }
-
-    private void FixedUpdate(){
-        if(mustPatrol){
-            mustTurn = !Physics2D.OverlapCircle(groundCheck.position, 1f, groundLayer);
-        }
-    }
-
-    void Patrol(){
-        if(mustTurn || bodyCollider.IsTouchingLayers(groundLayer)){
-            Flip();
-        }
-        myRigidbody.velocity = new Vector2(moveSpeed * Time.fixedDeltaTime, myRigidbody.velocity.y);
-    }
-
-    void Flip(){
-        mustPatrol = false;
-        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-        moveSpeed *= -1;
-        mustPatrol = true;
-
-    }*/
-
     public float speed;
     private bool movingRight = false;
     public Transform groundCheck;
