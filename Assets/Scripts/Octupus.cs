e7652067da5ca69e40b70c0fa61eefdbf692fc3f
FilipeABBarroso <filipebarroso2812@gmail.com>
2020-12-14 17:54:13 +0000

    extra life

--- Assets/Scripts/Octupus.cs
+++ Assets/Scripts/Octupus.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class Octupus : MonoBehaviour
 {
 
-    public int health = 100;
+    public int health = 100 + getExtraHealth;
     public GameObject deathEffect;
 
     public void TakeDamage(int damage)
@@ -24,5 +24,15 @@ public class Octupus : MonoBehaviour
         GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
         Destroy(effect, 0.45f);
+    }
+
+    private int getExtraHealth(){
+        Random r = new Random();
+        double v = r.NextDouble();
+        if(v <= 0.3) return 0;
+        else if (0.3 < v && v <= 0.6) return 20;
+        else if (0.6 < v && v <= 0.8) return 40;
+        else if (0.8 < v && v <= 0.95) return 60;
+        else return 90;
     }
 }
