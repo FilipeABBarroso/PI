b58893ee149d0c3e803ba047e1605977184d9503
TiagoMAGarcia <73131074+TiagoMAGarcia@users.noreply.github.com>
2020-12-14 23:43:39 +0000

    .

---
+++ Assets/Scripts/OctupusBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class OctupusBullet: MonoBehaviour
+{
+    public float Speed = 20f;
+    public int Damage = 40;
+    public GameObject impactEffect;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * Speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Player player = hitInfo.GetComponent<Player>();
+        if (player != null)
+        {
+            player.TakeDamage(Damage);
+        }
+        GameObject effect = Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+        Destroy(effect, 0.35f);
+    }
+}
